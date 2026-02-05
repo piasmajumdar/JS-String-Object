e301789ad28a82c489c58d86e51e797d0ef0320b
piasmajumdar <piasmajumdar@gmail.com>
2026-02-05 18:09:36 +0530

    object

---
+++ object.js
@@ -0,0 +1,30 @@
+// Primitve
+// basic, primary data type
+const age = 21;
+const school = 'rifle square';
+const isPassed = true;
+// let isDeveloper;
+
+const subjects = ['bangla', 'English', 'Physics', 'Math'];
+
+
+//Object 
+// non-primitive
+const bottle = {
+    brand: 'apple',
+    price: 45,
+    color: 'white',
+    isClean: false
+}
+
+const subject = {
+    name: 'biology',
+    teacher: 'rasheda mam',
+    examDate: '30 Feb',
+    chapters: ['first', 'second', 'third'],
+    exams: {
+        name: 'Final Exam',
+        marks: 100
+
+    }
+}
