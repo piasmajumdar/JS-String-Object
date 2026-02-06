137a54408f573b2047524eb30b5143ab40c6b0c5
piasmajumdar <piasmajumdar@gmail.com>
2026-02-06 20:06:01 +0530

    practice problem solved for Objects

---
+++ task_Object.js
@@ -0,0 +1,129 @@
+/* Task-1
+Access the golden rod color value in output.
+
+const colors = {
+    red: "#ff0000",
+    green: "#00ff00",
+    blue: "#0000ff",
+    "golden rod": '#daa520'
+}; */
+
+
+const colors = {
+    red: "#ff0000",
+    green: "#00ff00",
+    blue: "#0000ff",
+    "golden rod": '#daa520'
+};
+console.log(colors['golden rod']);
+
+// ----------------------------------------------------------------------------
+
+/* Task-2
+For this object below add a property named passenger capacity with value 5
+
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020
+};
+ */
+
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020
+};
+car['passenger capacity'] = 5;
+console.log(car);
+
+// ----------------------------------------------------------------------
+
+
+/* Task-3
+Display the physics marks as output.
+
+const student = {
+    name: "Hamim Sakep",
+    id: 5421,
+    physics: {
+        subject: "HSC Physics",
+        author: "Shahjahan Tapan",
+        marks: 30
+    }
+}; */
+
+
+const student = {
+    name: "Hamim Sakep",
+    id: 5421,
+    physics: {
+        subject: "HSC Physics",
+        author: "Shahjahan Tapan",
+        marks: 30
+    }
+};
+
+console.log('physics mark', ":", student.physics.marks);
+
+// -----------------------------------------------------------------------------
+
+/* Task-4
+Count the number of properties.
+
+Input:
+
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+}; */
+
+let student2 = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+
+console.log(Object.keys(student2).length);
+
+// ---------------------------------------------------------------
+
+
+/* Task-5 (Hard)
+Loop through an object and print the key-value pairs with their types
+
+Input:
+
+let myObject = {
+
+name: 'John Doe',
+age: 25,
+city: 'Example City',
+isStudent: true
+};
+
+Output:
+
+
+key: name | type:  string
+key: age | type:  number
+key: city | type:  string
+key: isStudent | type:  boolean
+ */
+
+let myObject = {
+    name: 'John Doe',
+    age: 25,
+    city: 'Example City',
+    isStudent: true
+};
+
+for(let prop in myObject) {
+    console.log('key: ', prop, ' | type: ', typeof myObject[prop])
+}
+
+
+
